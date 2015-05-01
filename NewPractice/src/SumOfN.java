a2780b1e86d77a317927fc1b5675e6b0f223b691
suruchisoni <suruchisoni@gmail.com>
2015-05-01 15:36:52 -0700

    ok

---
+++ NewPractice/src/SumOfN.java
@@ -0,0 +1,19 @@
+package Work;
+
+public class SumOfN {
+	
+	
+	public static void main(String args[]){
+				
+	   int s = 0;
+	 //nt i = 1;
+	 
+		for(int n=1; n<=5; n++)
+		{
+		s=n+s;
+		}
+		
+		System.out.println(s);
+	}
+
+}
