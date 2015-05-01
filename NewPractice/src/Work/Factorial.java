4ba11190002951b8b089ea9d10e7790028fa6ac0
suruchisoni <suruchisoni@gmail.com>
2015-05-01 14:58:44 -0700

    New project java basic programs

---
+++ NewPractice/src/Work/Factorial.java
@@ -0,0 +1,18 @@
+package Work;
+
+public class Factorial {
+	
+	public static void main(String args[])
+	{
+		int f=1;
+		for(int i=1; i<=3; i++)
+		{
+			
+			f=f*i;
+			
+		}
+		System.out.println(f);
+	}
+	
+
+}
---
+++ NewPractice/src/Work/PrimeNumbers.java
@@ -0,0 +1,28 @@
+package Work;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		
+			int i = 6;
+			
+			if (i%2==0){
+				System.out.println(" not a prime number");	
+				
+			}
+							
+			else {
+					System.out.println("prime number");	
+				}
+						
+				
+	}
+}
+			
+			
+			
+				
+
+	
+
+
