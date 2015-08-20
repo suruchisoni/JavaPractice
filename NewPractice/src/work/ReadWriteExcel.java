922db5b91b1846acb35b5b0f74e526a0911e2786
suruchisoni <suruchisoni@gmail.com>
2015-08-20 16:42:52 -0700

    added classes

--- NewPractice/src/work/ReadWriteExcel.java
+++ NewPractice/src/work/ReadWriteExcel.java
@@ -1,15 +1,22 @@
 package work;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import jxl.write.Label;
+import jxl.write.WritableWorkbook;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ReadWriteExcel {
 
@@ -49,9 +56,10 @@ public class ReadWriteExcel {
         }
        
       }
-@Test
-public void writeToExcel() throws IOException{
+
+  public void writeToExcel() throws IOException{
 	
+  
 	FileInputStream fis = new FileInputStream("C:\\Users\\Suruchi\\Documents\\Book1.xls");
 	HSSFWorkbook wb = new HSSFWorkbook(fis);
 	HSSFSheet s = wb.getSheetAt(2);
@@ -60,11 +68,52 @@ public void writeToExcel() throws IOException{
 	cell.setCellType(cell.CELL_TYPE_STRING);
 	cell.setCellValue("Selenium");
 	
+	
 	FileOutputStream fos = new FileOutputStream("C:\\Users\\Suruchi\\Documents\\Book1.xls");
 	wb.write(fos);
 	fos.close();
 	System.out.println("excel is written ");
 	
-}
-}
+      }
+
+  @Test
+  public void newmethod() throws IOException{
+	  
+	  FirefoxDriver driver;
+	  
+	  driver = new FirefoxDriver();
+	  
+	  driver.get("https://www.amazon.com");
+	  
+	  String a,b;
+	  
+	  File fExcel = new File("C:\\Users\\suruchi_soni\\Desktop\\Suruchi\\Selenium\\test\\Input.xls");
+      HSSFWorkbook wb = new HSSFWorkbook (); 
+      HSSFWorkbook wb =  HSSFWorkbook.create(fExcel);
+      wb.createSheet("Data");
+	  
+	  for(int i = 0; i < 3; i++) {
+	        int j = i + 1 ;
+	        a = driver.findElement(By.xpath(".//*[@id='nav-your-amazon']")).getText();
+	        System.out.println("data1 = " +  a);
+	        b = driver.findElement(By.xpath(".//*[@id='nav-xshop']/a[2]")).getText();
+	        System.out.println("data2 = " +  b);
+
+	        
+
+	        HSSFSheet sh = wb.getSheetAt(0);
+
+	        Label data1  = new Label(j, 1, a);
+	        sh.createRow(0);
+
+	        Label data2 = new Label(j, 1, b);
+	        sh.createRow(1);
+	        
+	        FileOutputStream fos = new FileOutputStream("C:\\Users\\suruchi_soni\\Desktop\\Suruchi\\Selenium\\test\\Input.xls");
+	        wb.write(fos);
+	        wb.close();               
+	    }
+  }
+    
+   }
 
---
+++ NewPractice/src/work/Test1.java
@@ -0,0 +1,24 @@
+package work;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Test1 {
+	
+	FirefoxDriver driver;
+	
+	@Test
+	public void testMethod(){
+		WebDriver driver = new FirefoxDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		driver.get("http://www.priceline.com/");
+		
+	}
+	
+	
+	
+	
+
+}
