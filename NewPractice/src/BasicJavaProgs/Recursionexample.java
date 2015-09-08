c2b5f41ec74452102ab17b8e21b147c5fa1f214f
suruchisoni <suruchisoni@gmail.com>
2015-09-08 16:49:34 -0700

    recursion

---
+++ NewPractice/src/BasicJavaProgs/Recursionexample.java
@@ -0,0 +1,28 @@
+package BasicJavaProgs;
+
+public class Recursionexample {
+
+		
+		public  long factorial(int n){
+		    if (n == 1)
+		        return 1;
+		    else
+		        return n * factorial(n-1);
+		}
+		
+			
+		     
+	    public String reverseString(String str){
+	    	String reverse = "";
+	        if(str.length() == 1){
+	            return str;
+	        } else {
+	            reverse = reverse + str.charAt(str.length()-1)
+	                    +reverseString(str.substring(0,str.length()-1));
+	            return reverse;
+	        }
+	    }
+	     
+	}
+
+
---
+++ NewPractice/src/test/work/RecursionTest.java
@@ -0,0 +1,16 @@
+package test.work;
+
+import BasicJavaProgs.Recursionexample;
+
+public class RecursionTest {
+
+	public static void main(String[] args) {
+		Recursionexample r = new Recursionexample();
+		System.out.println(r.factorial(5));
+		
+		 
+	      System.out.println("Result: "+r.reverseString("Name"));
+		
+	}
+
+}
